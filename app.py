913d843e2a8b9dfb451dfca0060c94e659760a0c
Henri Paasonen <hejupa035@gmail.com>
2021-02-28 20:52:17 +0200

    M

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/a")
+def a():
+    return render_template("a.html")
+
 if __name__ == "__main__":
     app.run()
