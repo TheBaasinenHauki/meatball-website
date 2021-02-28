2205d2db7cf18282b58ea597c39477b296e90d74
Henri Paasonen <hejupa035@gmail.com>
2021-02-28 14:11:00 +0200

    Ei jumalauta

--- bot.py
+++ bot.py
@@ -18,7 +18,6 @@ client = commands.Bot(command_prefix = '.')
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game('meatballbot.me'))
-    print("ready")
 
 @client.event
 async def on_command_error(ctx, error):
