82012f0b414dfc38642671416ffaa738ba2d8db1
TheBaasinenHauki <45098887+TheBaasinenHauki@users.noreply.github.com>
2021-03-19 20:24:31 +0200

    nyt

--- app.py
+++ app.py
@@ -6,7 +6,20 @@ from pydrive.drive import GoogleDrive
 import ast
 
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+# Try to load saved client credentials
+gauth.LoadCredentialsFile("mycreds.txt")
+if gauth.credentials is None:
+    # Authenticate if they're not there
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    # Refresh them if expired
+    gauth.Refresh()
+else:
+    # Initialize the saved creds
+    gauth.Authorize()
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("mycreds.txt")
+
 drive = GoogleDrive(gauth)
 
 
--- bot.py
+++ bot.py
@@ -8,9 +8,23 @@ import random
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import ast
+import os
 
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+# Try to load saved client credentials
+gauth.LoadCredentialsFile("mycreds.txt")
+if gauth.credentials is None:
+    # Authenticate if they're not there
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    # Refresh them if expired
+    gauth.Refresh()
+else:
+    # Initialize the saved creds
+    gauth.Authorize()
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("mycreds.txt")
+
 drive = GoogleDrive(gauth)
 
 with open('users.json', 'w') as k:
